fa093905a68804ac45b87c8ea7a6b4ef411b52e2
zehraimam02 <izehra70@gmail.com>
2024-10-09 11:23:52 +0500

    part 2 done

--- api_practise/lib/screen/home.dart
+++ api_practise/lib/screen/home.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:api_practise/model/user.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; //alias
 
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
 
   @override
   Widget build(BuildContext context) {
@@ -23,32 +23,16 @@ class _HomeScreenState extends State<HomeScreen> {
         itemCount: users.length,
         itemBuilder: (context,index){
           final user = users[index];
-          final name = user['name']['first'];
-          final email = user['email'];
-          final imageUrl = user['picture']['thumbnail'];
+          final color = user.gender == 'male'? const Color.fromARGB(255, 196, 229, 244): Colors.white;
 
-          print(imageUrl); // Check if the URL is valid
+          //print(email); // Check if the URL is valid
+
           return ListTile(
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.network(
-                imageUrl,
-                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child; // Image loaded successfully
-                  } else {
-                    return const CircularProgressIndicator(); // Placeholder while loading
-                  }
-                },
-                errorBuilder: (context, error, stackTrace) {
-                  return const Icon(Icons.error); // Show an error icon if the image fails to load
-                },
-              ),
-            ),
-
-            title: Text(name),
-            subtitle: Text(email),
+            title: Text(user.name.first),
+            subtitle: Text(user.phone),
+            tileColor: color,
           );
+
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -58,13 +42,37 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void fetchUsers() async{
+
+    print('fetchUsers called');
+
     const url = 'https://randomuser.me/api/?results=100';
     final uri = Uri.parse(url);
     final response = await http.get(uri); // waiting for the response, until not recieved prog will not move fwd
+    
     final body = response.body; // the response from the api will have many components like header, body etc..we want the body that appears on the browser screen
     final json = jsonDecode(body);//response will be in form of string, use json decode to parse the string and return it
+    final results = json['results'] as List<dynamic>;
+
+    final transformed = results.map((e) {
+      final name = UserName( 
+        title: e['name']['title'],
+        first: e['name']['first'],
+        last: e['name']['last'],
+      );
+      return User(
+        cell: e['cell'],
+        email: e['email'],
+        gender: e['gender'],
+        nat: e['nat'],
+        phone: e['phone'],
+        name: name,
+      );
+    }).toList();
     setState(() {
-      users = json['results']; // the key is results, like data array in the assignment, here we give the key
+      users = transformed; // the key is results, like data array in the assignment, here we give the key
     });
+    print('fetchUsers completed');
+
   }
+
 }
